655c4eee658de6dd960a8e7580f17028be2c78b4
Prasiddh Goel <74052423+prasiddhgoel@users.noreply.github.com>
2020-12-04 09:36:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,66 @@
-var car,wall
-var speed,weight
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  speed=random(55,90)
-  weight=random(400,1500)
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
-  createCanvas(800,800);
-  car=createSprite(50,200,50,50)
+	engine = Engine.create(packageBody);
+
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
 
 
-  car.velocityX=speed;
-  car.shapeColor=color(225)
-
+	Engine.run(engine);
   
-  wall=createSprite(700,200,60, height/2)
-  wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
-  background(255,255,255);  
-  if (wall.x-car.x < (car.width+wall.width)/2)
-  {
-  car.velocityX=0
-  var deformation=0.5 * weight * speed * speed/22509
-  if (deformation>180)
-  {
-  car.shapeColor=color(225,0,0) 
-  }
-  if (deformation<180 && deformation>100)
-  {
-    car.shapeColor=color(230,230,0)
-  }
-  if (deformation<100)
-  {
-  car.shapeColor=(0,225,0)
-  }
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+  drawSprites();
 }
-drawSprites()
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+matter.Body.setStatic(packageBody,false)
+packageSprite.x=packageSprite.position.x
+packageSprite.y=packageSprite.position.y
+  }
 }
 
