a93c6ec1cf702b8cd0c49985092784c5deb72231
Prasiddh Goel <74052423+prasiddhgoel@users.noreply.github.com>
2021-01-03 11:30:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,28 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+var dustbin1, dustbin2, dustbin3;
+var ground;
+var paper;
+
+function preload() {
+	
 }
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
+	createCanvas(1400,300);
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
-	engine = Engine.create(packageBody);
-
+	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
+	paper = new Paper (100,100,30);
+	ground = new Ground (700,280,1400,10);
+	dustbin1 = new Dustbin (1000,220,20,80);
+	dustbin2 = new Dustbin (1090,265,200,20)
+	dustbin3 = new Dustbin (1180,220,20,80);
 
 	Engine.run(engine);
   
@@ -47,21 +30,30 @@ function setup() {
 
 
 function draw() {
+  
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+
+  text ("prasiddh goel", 200,200,200)
+  paper.display();
+  ground.display();
+  dustbin1.display();
+  dustbin3.display();
+  dustbin2.display();
+  
   drawSprites();
  
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-Matter.Body.setStatic(packageBody,false)
-  }
-}
+function keyPressed(){
 
+	if (keyCode === RIGHT_ARROW){
+
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:250,y:-150})
+
+	}
+
+}
 
 
 
